1c1ad16f3ddb13f38ee822d8b4df9ef2a82ad82e
Andrew Tran <tran.andrew@outlook.com>
2025-02-04 20:03:37 -0500

    Fix header spacing

--- client/blog-client/src/components/Header.jsx
+++ client/blog-client/src/components/Header.jsx
@@ -7,38 +7,40 @@ function Header() {
 
   return (
     <header>
-      <div className="header-left">
-        <Link to="/" className="logo">
-          <img className="logo-img" src="/logo.png" />
-          Buhlog
-        </Link>
-        <span className="pronunciation prevent-select">(/bla:g/)</span>
-      </div>
+      <div id="header-cont">
+        <div className="header-left">
+          <Link to="/" className="logo">
+            <img className="logo-img" src="/logo.png" />
+            Buhlog
+          </Link>
+          <span className="pronunciation prevent-select">(/bla:g/)</span>
+        </div>
 
-      <nav>
-        {user ? (
-          <div className="header-right">
-            <span>
-              Hello, <Link to={`/users`}>{user.username}</Link>
-            </span>
-            <div>
-              <Link to={`/users`}>
-                <button>Your Account</button>
-              </Link>
-              <button onClick={logout}>Logout</button>
+        <nav>
+          {user ? (
+            <div className="header-right">
+              <span>
+                Hello, <Link to={`/users`}>{user.username}</Link>
+              </span>
+              <div>
+                <Link to={`/users`}>
+                  <button>Your Account</button>
+                </Link>
+                <button onClick={logout}>Logout</button>
+              </div>
             </div>
-          </div>
-        ) : (
-          <div className="header-right">
-            <Link to="/login">
-              <button>Login</button>
-            </Link>
-            <Link to="/signup">
-              <button>Sign Up</button>
-            </Link>
-          </div>
-        )}
-      </nav>
+          ) : (
+            <div className="header-right">
+              <Link to="/login">
+                <button>Login</button>
+              </Link>
+              <Link to="/signup">
+                <button>Sign Up</button>
+              </Link>
+            </div>
+          )}
+        </nav>
+      </div>
     </header>
   );
 }
